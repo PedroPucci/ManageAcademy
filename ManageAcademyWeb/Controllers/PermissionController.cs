cbde9718fbf7445c475262c5e294f5374cebb410
pedro.h.pucci <pedro.pucci@mutant.com.br>
2024-02-12 10:12:08 -0300

    Add project files.

---
+++ ManageAcademyWeb/Controllers/PermissionController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ManageAcademyWeb.Controllers
+{
+    [ApiController]
+    [Route("api/v1/user")]
+    public class PermissionController : Controller
+    {
+        //private readonly IUnitOfWorkService _serviceUoW;
+
+        //public PermissionController(IUnitOfWorkService serviceUoW)
+        //{
+        //    _serviceUoW = serviceUoW;
+        //}
+
+        //[HttpPost("add")]
+        //[ProducesResponseType(StatusCodes.Status200OK)]
+        //[ProducesResponseType(StatusCodes.Status400BadRequest)]
+        //[ProducesDefaultResponseType]
+        //public async Task<IActionResult> AddPermissionEntity([FromBody] PermissionDto permissionDto)
+        //{
+        //    var result = await _serviceUoW.UserService.AddUser(userDto);
+        //    return result.Success ? Ok(result) : BadRequest(result);
+        //}
+    }
+}
