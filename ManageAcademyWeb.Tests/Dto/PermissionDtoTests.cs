ab3847771ec5f1045143674ae4eedab8d5f055e1
pedro.h.pucci <pedro.pucci@mutant.com.br>
2024-02-15 20:23:49 -0300

    (feat)Atividade#6: Testes para PermissionDto e PermissionEntity

---
+++ ManageAcademyWeb.Tests/Dto/PermissionDtoTests.cs
@@ -0,0 +1,34 @@
+﻿using ManageAcademyWeb.Domain.Dto;
+using ManageAcademyWeb.Domain.Enum;
+
+namespace ManageAcademyWeb.Tests.Dto
+{
+    public class PermissionDtoTests
+    {
+        [Fact(DisplayName = "Create PermissionDto with valid values")]
+        public void CreateUserDto_WithValidValues()
+        {
+            // Arrange
+            var name = "permissao 1";
+            var description = "descrição 1";
+            var status = true;
+            var type = PermissionStatus.Active;
+
+            // Act
+            var permissionDto = new PermissionDto
+            {
+                Name = name,
+                Description = description,
+                Status = status,
+                Type = type
+            };
+
+            // Assert
+            Assert.NotNull(permissionDto);
+            Assert.Equal(name, permissionDto.Name);
+            Assert.Equal(description, permissionDto.Description);
+            Assert.Equal(status, permissionDto.Status);
+            Assert.Equal(type, permissionDto.Type);
+        }
+    }
+}
---
+++ ManageAcademyWeb.Tests/Entity/PermissionEntityTests.cs
@@ -0,0 +1,34 @@
+﻿using ManageAcademyWeb.Domain.Entity;
+using ManageAcademyWeb.Domain.Enum;
+
+namespace ManageAcademyWeb.Tests.Entity
+{
+    public class PermissionEntityTests
+    {
+        [Fact(DisplayName = "Create PermissionEntity with custom values")]
+        public void CreateUserEntity_WithCustomValues()
+        {
+            // Arrange
+            var name = "permissao 1";
+            var description = "descrição 1";
+            var status = true;
+            var type = PermissionStatus.Active;
+
+            // Act
+            var permissionEntity = new PermissionEntity(1)
+            {
+                Name = name,
+                Description = description,
+                Status = status,
+                Type = type
+            };
+
+            // Assert
+            Assert.NotNull(permissionEntity);
+            Assert.Equal(name, permissionEntity.Name);
+            Assert.Equal(description, permissionEntity.Description);
+            Assert.Equal(status, permissionEntity.Status);
+            Assert.Equal(type, permissionEntity.Type);
+        }
+    }
+}
