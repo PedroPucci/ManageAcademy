9c3306630c5e151684b55233ce633fc9a4975644
pedro.h.pucci <pedro.pucci@mutant.com.br>
2024-02-18 10:45:33 -0300

    (feat)Atividade#6: Adicionado o Id na classe BaseEntity

--- ManageAcademyWeb.Domain/Entity/BaseEntity.cs
+++ ManageAcademyWeb.Domain/Entity/BaseEntity.cs
@@ -1,7 +1,12 @@
-﻿namespace ManageAcademyWeb.Domain.Entity
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManageAcademyWeb.Domain.Entity
 {
     public abstract class BaseEntity
     {
+        [Key]
+        public int Id { get; private set; }
+
         public DateTime? DataCreate { get; private set; }
 
         protected BaseEntity()
